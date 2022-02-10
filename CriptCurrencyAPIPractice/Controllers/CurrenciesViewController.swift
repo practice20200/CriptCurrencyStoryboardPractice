846dddd5089a34e79d35aea10c89ebc3aa531230
practice20200 <wor9hard2018@gmail.com>
2022-02-10 13:11:52 -0800

    ist Commit Sorted files

---
+++ CriptCurrencyAPIPractice/Controllers/CurrenciesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CurrenciesViewController.swift
+//  CriptCurrencyAPIPractice
+//
+//  Created by Apple New on 2022-02-10.
+//
+
+import UIKit
+
+class CurrenciesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
