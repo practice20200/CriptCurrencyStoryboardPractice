299c32a091134f2cd166d2b357084fc45174b998
practice20200 <wor9hard2018@gmail.com>
2022-02-12 01:42:54 -0800

    Commit Added Header(index) in ViewController

--- CriptCurrencyAPIPractice/Controllers/ViewController.swift
+++ CriptCurrencyAPIPractice/Controllers/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout {
    
     
     var data = MainCollectionViewData.dataProvider()
+    var indextitle = ["Currency", "Coins", "Gold"]
     //@IBOutlet weak var btcPrice: UILabel!
 //    @IBOutlet weak var ethPrice: UILabel!
 //    @IBOutlet weak var usdPrice: UILabel!
@@ -51,11 +52,11 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout {
         
     }
     
-    //override func viewDidLayoutSubviews() {
-      //  super.viewDidLayoutSubviews()
-   //     collectionView.frame = view.bounds
- //   }
-    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        collectionView.frame = view.bounds
+//    }
+//
     
     
     override func viewWillAppear(_ animated: Bool) {
@@ -75,6 +76,8 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(MainCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        collectionView.register(HeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderCollectionReusableView.identifier)
         
         dateLabel.text = DateFormatters.dateForMatter(date: Date())
         
@@ -170,14 +173,20 @@ extension ViewController : UICollectionViewDelegate {
 }
 
 extension ViewController : UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    func numberOfSections(in tableView: UICollectionView) -> Int {
         return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let sectionData = data[section]
+        return sectionData.1.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MainCollectionViewCell
         
-        let item = data[indexPath.row]
+        let item = data[indexPath.section].1[indexPath.row]
         cell.currencyIconImage.image = item.currencyIcon
         cell.fullCurrencyTitle.text = item.fullCurrencyTitle
         cell.currencyTitle.text = item.currencyTitle
@@ -188,8 +197,28 @@ extension ViewController : UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let height = (collectionView.frame.height * 1/3)
+        let height = (collectionView.frame.height * 1/4)
         let width = (collectionView.frame.width * 1/2)-30
         return CGSize(width: width, height: height)
     }
+
+
+    
+    //=========Header
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: HeaderCollectionReusableView.identifier, for: indexPath) as? HeaderCollectionReusableView
+        else { return UICollectionReusableView()}
+
+        header.iconImage.image = data[indexPath.section].0.icon
+        header.titleLabel.text = data[indexPath.section].0.title
+        
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: 100, height: 20)
+    }
+    
+    
 }
